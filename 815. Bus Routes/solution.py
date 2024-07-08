3f11bb44f480a7925b32db928d3e8e3607615e6c
AlexSavizky <alexasa2@ac.sce.ac.il>
2024-07-08 20:20:58 +0300

    bus routes 1st try

---
+++ 815. Bus Routes/solution.py
@@ -0,0 +1,41 @@
+def num_buses_to_destination_v1(routes, source, target):
+    """
+    Initial intuitive solution
+    leet code submit result -> Time Limit Exceeded
+    """
+    if source == target:
+        return 0
+    result = help_recursion(routes, source, target,1)
+    if result:
+        return result
+    else:
+        return -1
+
+
+def help_recursion(routes, source, target, counter):
+    source_list = []
+    for i in range(len(routes)):
+        if source in routes[i] and target in routes[i]:
+            return counter
+        if source in routes[i]:
+            source_list.append(routes[i])
+    for i in source_list:
+        new_routes = all_combination_from_source(i, routes)
+        return help_recursion(new_routes, source, target, counter + 1)
+
+
+def all_combination_from_source(source, routes):
+    new_comb = []
+    no_change = routes
+    no_change.remove(source)
+    for i in routes:
+        if set(i) & set(source) and i != source:
+            new_comb.append(list(set(i + source)))
+            j = i
+            no_change.remove(j)
+    return new_comb + no_change
+
+
+routes = [[7,12],[4,5,15],[6],[15,19],[9,12,13]]
+source, target, counter = 15, 12, 1
+print(num_buses_to_destination_v1(routes, source, target))
